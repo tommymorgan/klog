dc825025b877574f6e10a5f5000faf98fb5ff181
Tommy Morgan <tommy@spredfast.com>
2014-11-05 20:24:43 +0000

    Added styles for beer colors

--- client/src/controller.js
+++ client/src/controller.js
@@ -3,7 +3,10 @@ var keezerControllers = angular.module("keezerControllers", ["lbServices"]);
 keezerControllers.controller("BeerListCtrl", ["$scope", "Beer", "Keg", function ($scope, Beer, Keg) {
 	$scope.beers = Beer.find({
 		filter: {
-      include: 'breweries'
+      include: 'breweries',
+      where: {
+        floated: null
+      }
     }
 	});
 
