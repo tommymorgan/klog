6753a18ed81971a13c2017d778d63b2e843f3290
Tommy Morgan <tommy@spredfast.com>
2014-11-05 21:16:46 +0000

    multiply beer color by 10

--- client/src/controller.js
+++ client/src/controller.js
@@ -1,18 +1,17 @@
 var keezerControllers = angular.module("keezerControllers", ["lbServices"]);
 
 keezerControllers.controller("BeerListCtrl", ["$scope", "Beer", "Keg", function ($scope, Beer, Keg) {
-	$scope.beers = Beer.find({
-		filter: {
-      include: 'breweries',
-      where: {
-        floated: null
-      }
-    }
-	});
-
-  $scope.kegs = Keg.find({
+  Keg.find({
     filter: {
       include: 'beer'
+    },
+    where: {
+      floated: null
     }
+  }).$promise.then(function (kegs){
+      kegs.forEach(function(keg) {
+        keg.beer.srm *= 10; 
+      });
+      $scope.kegs = kegs;  
   });
 }]);
