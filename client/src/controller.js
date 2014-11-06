9a66d385318b293606010862706547219add0fdb
Tommy Morgan <tommy@spredfast.com>
2014-11-06 00:06:24 +0000

    rename relation breweries to brewery

--- client/src/controller.js
+++ client/src/controller.js
@@ -1,17 +1,17 @@
-var keezerControllers = angular.module("keezerControllers", ["lbServices"]);
-
-keezerControllers.controller("BeerListCtrl", ["$scope", "Keg", function ($scope, Keg) {
-  Keg.find({
-    filter: {
-      include: 'beer'
-    },
-    where: {
-      floated: null
-    }
-  }).$promise.then(function (kegs){
-      kegs.forEach(function(keg) {
-        keg.beer.srm *= 10; 
-      });
-      $scope.kegs = kegs;  
-  });
+angular.module("keezerControllers", ["lbServices"])
+  .controller("BeerListCtrl", ["$scope", "Keg", function ($scope, Keg) {
+    Keg.find({
+      filter: {
+        include: ['beer']
+      },
+      where: {
+        floated: null
+      }
+    }).$promise.then(function (kegs){
+        kegs.forEach(function(keg) {
+          keg.beer.srm *= 10;
+          keg.volume = Math.ceil(keg.current_ml * 100  / 58673);
+        });
+        $scope.kegs = kegs;  
+    });
 }]);
