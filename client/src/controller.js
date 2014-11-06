07d4c5d1ca00336f6a03515b09f8066defb76953
Tommy Morgan <tommy@spredfast.com>
2014-11-06 21:54:06 +0000

    Add keg graph history

--- client/src/controller.js
+++ client/src/controller.js
@@ -1,5 +1,9 @@
 (function(){
-  var controllers = angular.module("keezerControllers", ["lbServices"]);
+  var controllers = angular.module("keezerControllers", ["lbServices", "n3-line-chart"]);
+  function formatData(flows) {
+    return 
+  }
+  
   controllers.controller("BeersListCtrl", ["$scope", "Keg", "Brewery", function ($scope, Keg, Brewery) {
     Keg.find({
       filter: {
@@ -48,6 +52,69 @@
       }
     }).$promise.then(function (responce) {
       $scope.keg = responce;
+    });
+  }]);
+  
+  controllers.controller("KegHistoryCtrl", ["$scope", "$routeParams", "Keg", function ($scope, $routeParams, Keg) {
+    Keg.history($routeParams).$promise.then(function (response) {
+      $scope.keg = response.data;
+      window.response = response;
+      var remainingMl = response.data.start_ml;
+      $scope.beerName = response.data.beer.name;
+//       var data = [{
+//         ml: remainingMl,
+//         poured: 0,
+//         date: new Date(response.data.tapped),
+//       }];
+      var data = [];
+      response.data.keg_flows.forEach(function(flow) {
+        remainingMl -= flow.ml;
+        
+        data.push({
+          ml: remainingMl,
+          poured: flow.ml,
+          date: new Date(flow.timestamp),
+        });
+      });
+      $scope.data = data;
+      $scope.currentVolume = remainingMl;
+      $scope.options = {
+        axes: {
+          x: {
+            type: "date",
+            key: "date"
+          },
+          y: {
+            type: "area",
+            min: 0,
+          },
+        },
+        series: [
+          {
+            y: "ml",
+            label: $scope.beerName,
+            color: "black",
+            axis: "y",
+            type: "line",
+            thickness: "2px",
+            id: "series_0"
+          },
+        ],
+        tooltip: {
+          mode: 'scrubber',
+          interpolate: true,
+          formatter: function(x, y, series) {
+            return y + "ml of "+$scope.beerName+" remaining";
+          },
+        },
+        drawLegend: false,
+        stacks: [],
+        lineMode: "linear",
+        tension: 0.7,
+        drawLegend: true,
+        drawDots: true,
+        columnsHGap: 5
+      };
     });
   }]);
 })();
