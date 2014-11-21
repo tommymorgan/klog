b13c8de71e1e69433bade63e57c252b07cc576c0
Ivan Slautyn <islautyn@softserveinc.com>
2014-11-21 17:16:59 -0600

    add save beer functionality

--- client/src/controllers/admin/entities_controller.js
+++ client/src/controllers/admin/entities_controller.js
@@ -1,35 +1,50 @@
 (function () {
   var appControllers = angular.module("adminControllers", ["lbServices"]);
+
+  appControllers.factory("entitiesMap",["Beer", "Brewery", "Keg", function (Beer, Brewery, Keg) {
+    return {
+        beer: {
+          model: Beer,
+          relation: "brewery"
+        }, 
+        brewery: {
+          model: Brewery,
+          relation: null
+        },
+        keg: {
+          model: Keg,
+          relation: "beer",
+        }
+    };
+  }]);
+
   appControllers.controller("AdminEntitiesListCtrl", 
-     ["$scope", "$routeParams", "Beer", "Brewery", "Keg", 
-     function ($scope, $routeParams, Beer, Brewery, Keg) {
-       var entitiesMap = {
-          beer: {
-            model: Beer,
-            relation: "brewery"
-          }, 
-          brewery: {
-            model: Brewery,
-            relation: null
-          },
-          keg: {
-            model: Keg,
-            relation: "beer",
-          }
-      };
+     ["$scope", "$routeParams", "entitiesMap", function ($scope, $routeParams, entitiesMap) {
         var entity = $routeParams.entity;
         $scope.entity = entity;
         entitiesMap[entity].model.find().$promise.then(function (response){
           $scope.entities = response;
         });
   }]);
+
   appControllers.controller("AdminEntityCreateCtrl", 
-     ["$scope", "$routeParams", "Beer", "Brewery", "Keg", function ($scope, $routeParams, Beer, Brewery, Keg) {
-      $scope.model = {};
+     ["$scope", "$routeParams", "entitiesMap", function ($scope, $routeParams, entitiesMap) {
+        var entity = $routeParams.entity;
+        $scope.model = {};
        
-      Brewery.find().$promise.then(function (response) {
-        $scope.breweries = response;
-      });
-     }]);
-   
+        entitiesMap.brewery.model.find().$promise.then(function (response) {
+          $scope.breweries = response;
+        });
+      
+        $scope.saveModel = function () {
+          debugger;
+          entitiesMap[entity].model.create($scope.model);
+        };
+
+        $scope.back = function () {
+          window.history.back();
+        }  
+
+
+    }]);
 })();
